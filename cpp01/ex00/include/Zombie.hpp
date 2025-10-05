417c0ba0d1e452ade8162779311a39e95511ad82
Yuxin29 <yuxin.1.wu@gmail.com>
2025-10-06 00:18:20 +0300

    Zombie defined

--- cpp01/ex00/include/Zombie.hpp
+++ cpp01/ex00/include/Zombie.hpp
@@ -10,4 +10,68 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
 
+#include <string>
+#include <iostream>
+
+// You have to determine in what case it’s better to allocate the zombies on the stack or heap.
+// Zombies must be destroyed when you don’t need them anymore. 
+// The destructor must print a message with the name of the zombie for debugging purposes.
+class Zombie
+{
+private:
+    std::string name;
+    
+public:
+    Zombie(std::string name);	//constructer funcion
+	~Zombie();			        //deconstructer
+
+    void announce(void);
+    Zombie* newZombie( std::string name ); // It creates a zombie, name it, and return it so you can use it outside of the functionscope.
+    void randomChump( std::string name );   //It creates a zombie, name it, and the zombie announces itself.
+}
+
+// class Account 
+// {
+// public:
+
+// 	typedef Account		t;
+
+// 	static int	getNbAccounts( void );  //totoal number of accounts
+// 	static int	getTotalAmount( void );
+// 	static int	getNbDeposits( void );
+// 	static int	getNbWithdrawals( void );
+// 	static void	displayAccountsInfos( void );
+
+// 	Account( int initial_deposit );	//constructer funcion
+// 	~Account( void );				//deconstructer function: print exit status and informations
+
+// 	void	makeDeposit( int deposit );
+// 	bool	makeWithdrawal( int withdrawal );
+// 	int		checkAmount( void ) const;
+// 	void	displayStatus( void ) const;
+
+// private:
+
+// 	//information of all accounts: class level
+// 	static int	_nbAccounts;
+// 	static int	_totalAmount;
+// 	static int	_totalNbDeposits;
+// 	static int	_totalNbWithdrawals;
+
+// 	//class level 
+// 	static void	_displayTimestamp( void );
+
+// 	//informaiton of one accoutn: object level
+// 	int				_accountIndex;
+// 	int				_amount;
+// 	int				_nbDeposits;
+// 	int				_nbWithdrawals;
+
+// 	Account( void );
+// };
+
+
+#endif
--- cpp01/ex00/src/Zombie.cpp
+++ cpp01/ex00/src/Zombie.cpp
@@ -10,4 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+void Zombie::announce(void)
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
