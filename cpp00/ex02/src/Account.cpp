ea0b7e47135e56bb373cd359a644d0d072055cc9
Yuxin Wu <yuwu@c2r6p11.hive.fi>
2025-10-05 16:55:34 +0300

    forlder strucutre

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -6,14 +6,14 @@
 /*   By: yuwu <yuwu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 18:11:36 by yuwu              #+#    #+#             */
-/*   Updated: 2025/10/04 18:23:52 by yuwu             ###   ########.fr       */
+/*   Updated: 2025/10/05 16:45:17 by yuwu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <ctime>
+#include <iomanip> //std::setfill, std::setw
 #include <iostream>
-#include <string>
 
 //private static attibute must be defined outside the class oncce
 int Account::_nbAccounts = 0;
@@ -21,8 +21,28 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+//example [19920104_091532]
+//tm_year: +1900
+// tm_mon: +1
+// setw(2) + setfill('0')_
+// std::tm* localtime(const std::time_t* timep);
+void	Account::_displayTimestamp( void )
+{   
+    std::time_t now = std::time(NULL);
+    std::tm *t = std::localtime(&now);
+
+    std::cout   << "[" 
+                << (t->tm_year + 1900)
+                << std::setw(2) << std::setfill('0') << (t->tm_mon + 1) 
+                << std::setw(2) << std::setfill('0') << t->tm_mday 
+                << "_" 
+                << std::setw(2) << std::setfill('0') << t->tm_hour 
+                << std::setw(2) << std::setfill('0') << t->tm_min 
+                << std::setw(2) << std::setfill('0') << t->tm_sec 
+                << "]";
+}
+
 //constructor
-// example print
 // [19920104_091532] index:4;amount:1234;created
 Account::Account(int initial_deposit)
 {
@@ -30,16 +50,17 @@ Account::Account(int initial_deposit)
     _nbAccounts += 1;
     _amount = initial_deposit;
     _totalAmount += initial_deposit;
-    //time stamp
+    _displayTimestamp();
     std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
 //deconstructore
-// example print 
 // [19920104_091532] index:3;amount:430;closed
 Account::~Account(void)
 {
-    //time stamp
+    _nbAccounts -= 1;
+    _totalAmount -= _amount;
+    _displayTimestamp();
     std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
@@ -48,7 +69,7 @@ Account::~Account(void)
 // [19920104_091532] index:5;p_amount:0;deposit:23;amount:23;nb_deposits:1
 void	Account::makeDeposit(int deposit)
 {
-    //time stamp
+    _displayTimestamp();
     std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit" << deposit;
     _amount += deposit;
     _nbDeposits += 1;
@@ -65,12 +86,12 @@ bool	Account::makeWithdrawal(int withdrawal)
         std::cout << "not enough deposit to withdraw." << std::endl;
         return false;
     }
-    //time stamp
+    _displayTimestamp();
     std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal" << withdrawal;
     _amount -= withdrawal;
     _nbWithdrawals += 1;
     _totalAmount -= withdrawal;
-    _totalNbWithdrawals -= 1;
+    _totalNbWithdrawals += 1;
     std::cout << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
     return true;
 }
@@ -83,8 +104,10 @@ int		Account::checkAmount(void) const
 //[19920104_091532] index:1;amount:785;deposits:1;withdrawals:1
 void	Account::displayStatus(void) const
 {
-	//time stap;
-    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawal" << _nbWithdrawals << std::endl;
+	_displayTimestamp();
+    std::cout   << "index:" << _accountIndex << ";amount:" << _amount 
+                << ";deposits:" << _nbDeposits << ";withdrawal" << _nbWithdrawals 
+                << std::endl;
 }
 
 //***********************class level***********************
@@ -111,6 +134,8 @@ int	Account::getNbAccounts(void)
 //[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
  void	Account::displayAccountsInfos(void)
 {   
-     //time stamp
-    std::cout << "accounts:" << _nbAccounts << "total:" << _totalAmount << "deposits:"<< _totalNbDeposits << "withdrawals:" << _totalNbWithdrawals << std::endl;
+    _displayTimestamp();
+    std::cout   << "accounts:" << _nbAccounts << ";total:" << _totalAmount 
+                << ";deposits:"<< _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals 
+                << std::endl;
 }
