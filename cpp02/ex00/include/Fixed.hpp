866bcf04e6917636482712609865b0c2f53732ad
Mikko <yuxin.29.wu@gmail.com>
2025-10-12 22:39:32 +0300

    learning OCF and fixed

---
+++ cpp02/ex00/include/Fixed.hpp
@@ -0,0 +1,28 @@
+
+#ifndef FIX_H
+#define FIX_H
+
+#include <string>
+#include <iostream>
+
+// Orthodox Canonical Form,
+// unless explicitely stated otherwise. 
+// Then, they will implement the four required member
+// functions below:
+// • Default constructor
+// • Copy constructor
+// • Copy assignment operator
+// • Destructor
+class Fix
+{
+private:
+    //value
+
+public:
+    Fix();
+    //copy
+    //copy assine
+    ~Fixed(); 
+}
+#endif
+
--- cpp02/ex00/src/main.cpp
+++ cpp02/ex00/src/main.cpp
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "Fixed_hpp"
+
+#include <iostream>
+
+int main( void )
+{
+    Fixed a;        //1 obj
+    //Default constructor called
+    Fixed b( a );   //2 obj     -->>copy = first copy, then asigne????
+    //Copy constructor called  
+    //Copy assignment operator called 
+    //getRawBits member function called
+    Fixed c;        //3 obj
+    //Default constructor called
+    c = b;
+    //Copy assignment operator called
+    //getRawBits member function called
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    // getRawBits member function called
+    // 0
+    // getRawBits member function called
+    // 0
+    // getRawBits member function called
+    // 0
+    //Destructor called
+    // Destructor called
+    // Destructor called
+    return 0;
+}
