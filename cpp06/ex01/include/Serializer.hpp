e21c11329dd1c56627c04a6e6a8536dd58743807
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-01-07 15:54:54 +0200

    cpp06 mostly done

---
+++ cpp06/ex01/include/Serializer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <cstdint>
+//uintptr_t is an unsighned int type that guaranteed to be able to hold a ptr without losing info
+
+struct Data{
+    int n;
+    std::string str;
+};
+
+class Serializer
+{
+private:
+    // can not be initializable
+    Serializer() {}
+
+public:
+    //It takes a pointer and converts it to the unsigned integer type uintptr_t.
+    static uintptr_t serialize(Data* ptr);
+
+    //It takes an unsigned integer parameter and converts it to a pointer to Data.
+    static Data* deserialize(uintptr_t raw);
+};
---
+++ cpp06/ex02/src/Base.cpp
@@ -0,0 +1,51 @@
+
+#include "Base.hpp"
+
+//It randomly instantiates A, B, or C and returns the instance as a Base pointer. 
+Base* generate(void){
+    int r = std::rand() % 3;
+    if (r == 0)
+        return new A;
+    else if (r == 1)
+        return new B; 
+    return new C;
+}
+
+//It prints the actual type of the object pointed to by p: "A", "B", or "C".
+// with a ptr, it will return a nullptr if it is a bad cast
+void identify(Base* p){
+    if (dynamic_cast<A *>(p))
+        std::cout << "A\n";
+    else if (dynamic_cast<B *>(p))
+        std::cout << "B\n";
+    else if (dynamic_cast<C *>(p))
+        std::cout << "C\n";
+    else 
+        std::cout << "unknown\n";
+}
+
+//It prints the actual type of the object referenced by p: "A", "B", or "C". 
+// Using a pointer inside this function is forbidden.
+// with a ref, it will return a bad cast it is not the correct type.
+void identify(Base& p){
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A\n";
+        return;
+    }
+    catch (std::bad_cast& e){}
+    //in case of bad case, it jumpes to next try
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B\n";
+        return;
+    }
+    catch (std::bad_cast& e){}
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B\n";
+        return;
+    }
+    catch (std::bad_cast& e){}
+    std::cout << "unknown\n";
+}
